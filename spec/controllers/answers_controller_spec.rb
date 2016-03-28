3c4ab61369ab8a57bd73e18f4c159b30b057b522
Dmitry Starchevsky <sdima2@ya.ru>
2016-03-29 00:21:36 +0300

    fix(controllers) Fix last to comments about passing question_id and block in spec

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -3,37 +3,37 @@ require 'rails_helper'
 RSpec.describe AnswersController, type: :controller do
   let(:question) { create(:question) }
   describe 'GET #new' do
-  	before { get :new, question_id: question }
-  	it 'assigns a new Answer to @answer' do
-  		expect(assigns(:answer)).to be_a_new(Answer)
-  	end
+    before { get :new, question_id: question }
+    it 'assigns a new Answer to @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
 
-  	it 'renders new view' do
-  		expect(response).to render_template :new
-  	end
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
   end
 
-	describe 'POST #create' do
+  describe 'POST #create' do
     context 'with valid attributes' do
-	    let (:answer) { build :answer, question_id: question }
-		 	it 'saves the new answer in the database' do
-        expect { post :create, question_id: question, answer: answer.attributes }.to change{question.answers.count}.by(1)
-		 	end
-		 	it 'redirect to show view' do
-        post :create, question_id: question, answer: answer.attributes
-		 		expect(response).to redirect_to question_path(assigns(:question))
-		 	end
-		end
+      let (:answer) { build :answer, question_id: question }
+      it 'saves the new answer in the database' do
+        expect { post :create, question_id: question, answer: answer.attributes }.to change(question.answers, :count).by(1)
+      end
+      it 'redirect to show view' do
+      post :create, question_id: question, answer: answer.attributes
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
 
-		context 'with invalid attributes' do
-			let (:answer) { build :invalid_answer, question_id: question.id }
-			it 'does not save the answer' do
-				expect { post :create, question_id: question, answer: answer.attributes }.to_not change(Answer, :count)
-			end
-			it 're-redirect new view' do
-				post :create, question_id: question, answer: answer.attributes
-				expect(response).to render_template :new
-			end
-		end
-	end
+    context 'with invalid attributes' do
+      let (:answer) { build :invalid_answer, question_id: question.id }
+        it 'does not save the answer' do
+        expect { post :create, question_id: question, answer: answer.attributes }.to_not change(Answer, :count)
+      end
+      it 're-redirect new view' do
+        post :create, question_id: question, answer: answer.attributes
+        expect(response).to render_template :new
+      end
+    end
+  end
 end
