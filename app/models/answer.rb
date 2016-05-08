01a7ba5870a2f469fa0f6520a97339c099d0b793
Dmitry Starchevsky <sdima2@ya.ru>
2016-05-08 15:51:59 +0300

    Polymorphic attachments. Answers associated with attachments

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,12 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
+
+  has_many :attachments, as: :attachable
+
   validates :body, :question_id, :user_id, presence: true
+
+  accepts_nested_attributes_for :attachments
 
   scope :is_best, -> { where(is_best: true) }
   scope :best_first, -> { order('is_best desc, created_at asc') }
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,8 +3,13 @@ require 'rails_helper'
 RSpec.describe Answer, type: :model do
   it { should validate_presence_of :body }
   it { should validate_presence_of :question_id }
+
   it { should belong_to :question }
   it { should belong_to :user }
+
+  it { should have_many :attachments }
+
+  it { should accept_nested_attributes_for :attachments}
 
   describe '#set_best' do
     let(:question) { create(:question) }
